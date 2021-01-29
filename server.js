33732277720cd13c6d26f6749f1a6cafbcc9dfc7
Kara Schmidt <kara.jane.schmidt@gmail.com>
2021-01-29 16:42:15 -0500

    more boilerplate

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const logger = require("morgan");
+
+const PORT = 3000;
+
 const app = express();
-const PORT = process.env.PORT || 3000
+
+app.use(logger("dev"));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+// routes
+app.use(require("./routes/api.js"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
