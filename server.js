e9daa0e639a93b4f67ed9e8d0ba15cb73fd78116
Kara Schmidt <kara.jane.schmidt@gmail.com>
2021-02-01 17:12:27 -0500

    fixing port typo

--- server.js
+++ server.js
@@ -2,28 +2,29 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
-const PORT = process.env.PORT || 3000
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(logger("dev"));
 
 app.use(compression());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use(express.json());
 
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 }, (err) => {
-    if (err) throw err;
-    console.log("Mongo connected")
+  if (err) throw err;
+  console.log("Mongo connected")
 })
 
 // routes
